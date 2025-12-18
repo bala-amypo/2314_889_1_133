51b795eaeca137b1bd36d074bf3546f70d18b900
bala-amypo <bala@amypo.in>
2025-12-18 08:01:06 +0000

    User commit

--- src/main/java/com/example/demo/entity/Product.java
+++ src/main/java/com/example/demo/entity/Product.java
@@ -1,3 +1,14 @@
+package com.example.demo.entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta
 public class Product{
-    
+    @Id
+    private Long id;
+    @Column(name=unique)
+    @
+    private String sku;
+    private String name;
+    private String category;
+    private boolean active = true;
 }
