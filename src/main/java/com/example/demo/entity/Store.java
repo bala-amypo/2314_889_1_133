7729b4ce8791ee645b9a6b50a9b6e9cb328452ef
bala-amypo <bala@amypo.in>
2025-12-18 05:06:53 +0000

    User commit

--- src/main/java/com/example/demo/entity/Store.java
+++ src/main/java/com/example/demo/entity/Store.java
@@ -1,7 +1,23 @@
+import jakarta.persistence.*;
+
+@Entity
 public class Store{
+    @Id
     private Long id;
     private String storeName;
     private String address;
     private String region;
-    private boolean active;
+    private boolean active = true;
+
+    public class Store(Long id,String storeName,String address,String region,boolean active){
+        this.id = id;
+        this.storeName = storeName;
+        this.address = address;
+        this.region = region;
+        this.active = active;
+    }
+
+    public Long setName(){
+        this.store
+    }
 }
