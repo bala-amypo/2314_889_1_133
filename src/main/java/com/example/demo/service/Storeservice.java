514b2cc74872aff5b7908de67bf9aa1367843eb6
bala-amypo <bala@amypo.in>
2025-12-18 06:01:52 +0000

    User commit

--- src/main/java/com/example/demo/service/Storeservice.java
+++ src/main/java/com/example/demo/service/Storeservice.java
@@ -1,7 +1,42 @@
-package com.example.demo.service; 
-import com.example.demo.entity.Store; 
+package com.example.demo.service;
+
+import com.example.demo.entity.Store;
 import java.util.*;
-import com.example.demo.service.Storeservice; 
- @Service 
- public class Storeservice{
-     private final Storerepo rep; public Storeservice(Storerepo rep){ this.rep = rep; } public Store savedata(Store newfile){ return rep.save(newfile); } @Override public Store getIdvalue(Long id){ return rep.findById(id); } @Override public List<Store> getall(){ return rep.findAll(); } @Override public Store update(Long id,Store newfile){ Store existing = rep.findById(id).orElse(null); if (existing != null) { existing.setName(newfile.getName()); existing.setEmail(newfile.getEmail()); return rep.save(existing); } return null; } @Override public void del(Long id){ rep.deleteById(id); } }
+import com.example.demo.service.Storeservice;
+import com.example.demo.repository.Storerepo;
+@Service
+public class Storeservice{
+
+    private final Storerepo rep;
+
+    public Storeservice(Storerepo rep){
+        this.rep = rep;
+    }
+    
+    public Store savedata(Store newfile){
+        return rep.save(newfile);
+    }
+    
+    public Store getIdvalue(Long id){
+        return rep.findById(id);
+    }
+    
+    public List<Store> getall(){
+        return rep.findAll();
+    }
+    
+    public Store update(Long id,Store newfile){
+        Store existing = rep.findById(id).orElse(null);
+
+        if (existing != null) {
+            existing.setName(newfile.getName());
+            existing.setEmail(newfile.getEmail());
+            return rep.save(existing);
+        }
+        return null;
+    }
+    @Override
+    public void del(Long id){
+        rep.deleteById(id); 
+    }
+}
