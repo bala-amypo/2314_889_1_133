1b2f5a91911d7648adf70d26043cb96b044beb82
bala-amypo <bala@amypo.in>
2025-12-18 09:12:33 +0000

    User commit

--- src/main/java/com/example/demo/entity/InventoryLevel.java
+++ src/main/java/com/example/demo/entity/InventoryLevel.java
@@ -1,4 +1,10 @@
 package com.example.demo.entity;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.*;
 public class InventoryLevel{
+    @Id
     private Long id;
+    @PositiveOrZero
+    private Integer quantity;
+    private LocalDate lastUpdated;
 }
