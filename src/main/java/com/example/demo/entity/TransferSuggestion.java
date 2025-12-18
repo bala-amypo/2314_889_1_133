5783e76b2394271a32986eee269b409dd11ea56e
bala-amypo <bala@amypo.in>
2025-12-18 09:37:32 +0000

    User commit

--- src/main/java/com/example/demo/entity/TransferSuggestion.java
+++ src/main/java/com/example/demo/entity/TransferSuggestion.java
@@ -1,5 +1,11 @@
 package com.example.demo.entity;
-
+import java.time.LocalDateTime;
+import  jakarta.persistence.Id;
 public class TransferSuggestion{
-    
+    @Id
+    private Long id;
+    private Integer quantity;
+    private String priority;
+    private LocalDateTime suggestedAt;
+    private String status;
 }
