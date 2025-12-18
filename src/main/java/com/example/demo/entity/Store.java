9d7702ef84588bb5cf0802a7b1d971c417bb8507
bala-amypo <bala@amypo.in>
2025-12-18 07:55:26 +0000

    User commit

--- src/main/java/com/example/demo/entity/Store.java
+++ src/main/java/com/example/demo/entity/Store.java
@@ -2,10 +2,12 @@ package com.example.demo.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 
 @Entity
+@Table (name = "Store")
 public class Store {
 
     @Id
