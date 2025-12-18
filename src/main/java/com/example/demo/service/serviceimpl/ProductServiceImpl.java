f4a2a1b9fdd98f850ca89dacb5670aa23845fd28
bala-amypo <bala@amypo.in>
2025-12-18 16:23:48 +0000

    User commit

--- src/main/java/com/example/demo/service/serviceimpl/ProductServiceImpl.java
+++ src/main/java/com/example/demo/service/serviceimpl/ProductServiceImpl.java
@@ -1,2 +1,14 @@
 package com.example.demo.service.serviceimpl;
 import com.example.demo.entity.Product;
+import com.example.demo.repository.ProductRepository;
+import com.example.demo.service.ProductService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService{
+    private final ProductRepository productrepo;
+
+    public ProductServiceImpl()
+}
