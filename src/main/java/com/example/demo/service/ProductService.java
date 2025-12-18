5e3b19fc98794989cc0c3d601e19f7e3ed61a48b
bala-amypo <bala@amypo.in>
2025-12-18 15:17:49 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -0,0 +1,8 @@
+package com.example.demo.service;
+import java.util.List;
+import java.util.Optional;
+import com.example.demo.entity.Product;
+
+public interface Product{
+    public Product createProduct(Product)
+}
--- src/main/java/com/example/demo/service/StoreService.java
+++ src/main/java/com/example/demo/service/StoreService.java
@@ -1,9 +1,8 @@
 package com.example.demo.service;
 import java.util.List;
-import java.util.Optional;
 import com.example.demo.entity.Store;
 
-public interface StudentService{
+public interface Store{
     Store createStore(Store store);
     Store getStoreById(Long id);
     List<Store> getAllStores();
