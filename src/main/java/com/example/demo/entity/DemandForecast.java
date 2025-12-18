de1e0d481d74cf5f256c2dd5a373c33099b1c32a
bala-amypo <bala@amypo.in>
2025-12-18 09:52:32 +0000

    User commit

--- src/main/java/com/example/demo/entity/DemandForecast.java
+++ src/main/java/com/example/demo/entity/DemandForecast.java
@@ -1,3 +1,13 @@
-package com.example.demo.entity
+package com.example.demo.entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
 
-public class 
+
+public class DemandForecast{
+    @Id
+    private Long id;
+    private LocalDate forecastDate;
+    @Column(nullable = false)
+    private Integer predictedDemand;
+    private Double confidenceScore;
+}
