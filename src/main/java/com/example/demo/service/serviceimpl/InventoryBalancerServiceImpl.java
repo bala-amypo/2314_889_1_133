c49a83cb235951d042afd6e079cc2a38671bfbd7
bala-amypo <bala@amypo.in>
2025-12-18 17:08:48 +0000

    User commit

--- src/main/java/com/example/demo/service/serviceimpl/InventoryBalancerServiceImpl.java
+++ src/main/java/com/example/demo/service/serviceimpl/InventoryBalancerServiceImpl.java
@@ -2,4 +2,14 @@ package com.example.demo.service.impl;
 import com.example.demo.entity.InventoryLevel;
 import com.example.demo.repository.InventoryLevelRepository;
 import com.example.demo.service.InventoryBalancerService;
-import org.springframework.stereo
+import org.springframework.stereotype,Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class InventoryBalanceServiceImpl
+implements InventoryBalancerService{
+    private final InventoryLevelRRepoitory inventoryRepository;
+    public InventoryBalancerServiceImpl(InventoryLevelRepository invntory)
+}
