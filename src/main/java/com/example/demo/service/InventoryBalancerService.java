17177712710bc6e011dc30a9e5a369d166f8d26a
bala-amypo <bala@amypo.in>
2025-12-18 15:32:49 +0000

    User commit

--- src/main/java/com/example/demo/service/InventoryBalancerService.java
+++ src/main/java/com/example/demo/service/InventoryBalancerService.java
@@ -3,5 +3,7 @@ import java.util.List;
 import com.example.demo.entity.InventoryLevel;
 
 public interface InventoryLevelBalancer{
-    Li
+    List<InventoryLevel> generateSuggestions(Long productId);
+    List<InventoryLevel> generateSuggestionsForStore(Long storeId);
+    InventoryLevel getSuggestionById(Long id);
 }
