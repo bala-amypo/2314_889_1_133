4bb8ef0ba553956544ba8798f1a69ab9e0f6fe3a
bala-amypo <bala@amypo.in>
2025-12-18 16:47:48 +0000

    User commit

--- src/main/java/com/example/demo/service/serviceimpl/DemandForecastServiceImpl.java
+++ src/main/java/com/example/demo/service/serviceimpl/DemandForecastServiceImpl.java
@@ -1,4 +1,12 @@
 package com.example.demo.service.serviceimpl;
 
 import com.example.demo.entity.DemandForecast;
-import
+import com.example.demo.repository.DemandForecastRepository;
+import com.example.demo.service.DemandForecastService;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Service
+
+public class DemandForecastServiceImpl implements
