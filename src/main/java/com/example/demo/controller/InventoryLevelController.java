62efdaa8f2db0b6a58e99e47893a3d8d49e318da
bala-amypo <bala@amypo.in>
2025-12-19 04:21:31 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/InventoryLevelController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.entity.InventoryLevel;
+import com.example.demo.service.InventoryLevelService;
+import java.util.*;
+@RestController
+@RequestMapping("/api/inventory")
+
+public class InventoryLevelController{
+    @Autowired
+    ProductService src;
+    @PostMapping
+    public Product Createproduct(@RequestBody Product st){
+        return src.createProduct(st);
+    }
+    @GetMapping
+    public List<Product> Listproducts(){
+        return src.getAllProducts();
+    }
+    @GetMapping("/{id}")
+    public Product Getproduct(@PathVariable Long id){
+        return src.getProductById(id);
+
+    }
+}
