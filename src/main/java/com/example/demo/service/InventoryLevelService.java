c080cd70773e426163a9e40d9e35a70977dfa19a
bala-amypo <bala@amypo.in>
2025-12-25 14:59:39 +0000

    User commit

--- src/main/java/com/example/demo/service/InventoryLevelService.java
+++ src/main/java/com/example/demo/service/InventoryLevelService.java
@@ -1,8 +1,8 @@
 package com.example.demo.service;
 
-import com.example.demo.entity.Store;
+import com.example.demo.entity.InventoryLevel;
 import com.example.demo.exception.ResourceNotFoundException;
-import com.example.demo.repository.StoreRepository;
+import com.example.demo.repository.InventoryLevelRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
