b2d7dcdfda7f61ac4f90c87b4aa6571fb053cf2e
bala-amypo <bala@amypo.in>
2025-12-26 08:43:30 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -71,7 +71,7 @@
 // }
 
 
-package com.example.OneToMany.config;
+package com.example.demo.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -83,7 +83,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.example.security.JwtFilter;
+import com.example.demo.security.JwtFilter;
 
 @Configuration
 @EnableMethodSecurity
