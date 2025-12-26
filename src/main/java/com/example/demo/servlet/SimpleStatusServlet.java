794c1280402f24307c6c4b8db2fc0d853fbe376e
bala-amypo <bala@amypo.in>
2025-12-26 13:44:01 +0000

    User commit

--- src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
+++ src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 public class SimpleStatusServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         // Test cases t01-t03 expect SC_OK (200)
         resp.setStatus(HttpServletResponse.SC_OK);
         
