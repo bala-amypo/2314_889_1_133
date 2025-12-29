14b255a0d79c3a381982967e15662667b85f96d8
bala-amypo <bala@amypo.in>
2025-12-29 08:52:54 +0000

    User commit

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -1,45 +1,9 @@
 package com.example.demo.controller;
-
 import com.example.demo.entity.Product;
 import com.example.demo.service.ProductService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-
-// @RestController
-// @RequestMapping("/api/products")
-// public class ProductController {
-
-//     private final ProductService service;
-
-//     public ProductController(ProductService service) {
-//         this.service = service;
-//     }
-
-//     @PostMapping
-//     @ResponseStatus(HttpStatus.CREATED)
-//     public Product create(@RequestBody Product product) {
-//         return service.createProduct(product);
-//     }
-
-//     @GetMapping("/{id}")
-//     public Product get(@PathVariable Long id) {
-//         return service.getProductById(id);
-//     }
-
-//     @GetMapping
-//     public List<Product> getAll() {
-//         return service.getAllProducts();
-//     }
-
-//     @DeleteMapping("/{id}")
-//     @ResponseStatus(HttpStatus.NO_CONTENT)
-//     public void deactivate(@PathVariable Long id) {
-//         service.deactivateProduct(id);
-//     }
-// }
-
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
--- src/main/java/com/example/demo/controller/StoreController.java
+++ src/main/java/com/example/demo/controller/StoreController.java
@@ -1,50 +1,9 @@
 package com.example.demo.controller;
-
 import com.example.demo.entity.Store;
 import com.example.demo.service.StoreService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
-
-// @RestController
-// @RequestMapping("/api/stores")
-// public class StoreController {
-
-//     private final StoreService service;
-
-//     public StoreController(StoreService service) {
-//         this.service = service;
-//     }
-
-//     @PostMapping
-//     @ResponseStatus(HttpStatus.CREATED)
-//     public Store create(@RequestBody Store store) {
-//         return service.createStore(store);
-//     }
-
-//     @GetMapping("/{id}")
-//     public Store get(@PathVariable Long id) {
-//         return service.getStoreById(id);
-//     }
-
-//     @GetMapping
-//     public List<Store> getAll() {
-//         return service.getAllStores();
-//     }
-
-//     @PutMapping("/{id}")
-//     public Store update(@PathVariable Long id, @RequestBody Store store) {
-//         return service.updateStore(id, store);
-//     }
-
-//     @DeleteMapping("/{id}")
-//     @ResponseStatus(HttpStatus.NO_CONTENT)
-//     public void deactivate(@PathVariable Long id) {
-//         service.deactivateStore(id);
-//     }
-// }
-
 @RestController
 @RequestMapping("/api/stores")
 public class StoreController {
